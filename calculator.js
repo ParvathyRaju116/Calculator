e0a1c916998472fff693c29529a6803e5d28f80e
unknown <parvathyraju550@gmail.com>
2023-09-13 10:06:15 +0530

    initial commit

---
+++ calculator.js
@@ -0,0 +1,12 @@
+function displayData(data){
+    result.value+=data
+}
+function allclrear(){
+    result.value=""
+}
+function findOut(){
+    result.value=eval(result.value)
+}
+function backspace(){
+    result.value=result.value.slice(0,-1)
+}
